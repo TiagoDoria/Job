cdd451b8dd993cd51a48e800b5e803cb4e0772d4
TiagoDoria <tiagodoriap@gmail.com>
2022-09-07 14:59:31 -0300

    Criando controller Candidite e recriando projeto

---
+++ ApiJob/Controllers/CandidateController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using JobDomain.Dtos.Candidate;
+using JobDomain.Core.Interfaces.Services;
+
+namespace JobApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CandidateController : ControllerBase
+    {
+        private ICandidateService _candidateService;
+
+        public CandidateController(ICandidateService candidateService)
+        {
+            _candidateService = candidateService;
+        }
+
+        // POST: CandidateController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([FromBody] CandidateDto candidate)
+        {
+            try
+            {
+                _candidateService.AddAsync(candidate);
+                return Ok();
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+
+        // POST: CandidateController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public void Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                //return Ok();
+            }
+            catch
+            {
+                ;
+            }
+        }
+
+
+        // POST: CandidateController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public void Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // return Ok();
+            }
+            catch
+            {
+                ;
+            }
+        }
+    }
+}
